fbc6efc18b42dcbf8afbbeb607ce56dd9eef673b
Roberto Delgado <bobyelboby@gmail.com>
2021-11-16 17:57:06 -0500

    uptaded motor controller and main2 to drive car

--- motor_controller.py
+++ motor_controller.py
@@ -14,33 +14,48 @@ def left():
     board.digital[in3].write(0)
     board.digital[in4].write(1)
     
-def forward():
+def reverse():
     board.digital[in1].write(0)
     board.digital[in2].write(1)
     board.digital[in3].write(1)
     board.digital[in4].write(0)
         
-def reverse():
+def forward():
     board.digital[in1].write(1)
     board.digital[in2].write(0)
     board.digital[in3].write(0)
     board.digital[in4].write(1)
 
+def stop():
+    enA.write(0)
+    enB.write(0)
+
+def run():
+    enA.write(0.5)
+    enB.write(0.5)
+
 # if __name__ == '__main__':
 
-# board = pyfirmata.Arduino('/dev/ttyACM0')
+board = pyfirmata.Arduino('/dev/ttyUSB1')
 
 led = 13
 in1 = 9
 in2 = 8
 in3 = 3
 in4 = 4
-#enA = board.digital[10]
-#enA.mode = pyfirmata.PWM
-#enB = board.digital[5]
-#enB.mode = pyfirmata.PWM
+enA = board.digital[10]
+enA.mode = pyfirmata.PWM
+enB = board.digital[5]
+enB.mode = pyfirmata.PWM
+# try:
+#     while True:
+#     # board.digital[led].write(0)
+#     # sleep(1)
+#     # board.digital[led].write(1)
+#     # sleep(1)
+#         right()
+#         enA.write(0.5)
+#         enB.write(0.5)
+# except KeyboardInterrupt:
+#     stop()
 
-#while True:
-#    right()
-#    enA.write(0.5)
-#    enB.write(0.5)
